c5a851139ee094a69b77e00bbabf03a543168c71
dcchris45 <dccomics65@gmail.com>
2025-12-01 02:54:57 +0100

    Basic Arithmetic Exercise

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -1,6 +1,9 @@
 print("hello world") 
 number1 = 10
-number2 = 5
-print(number1 + number2)
-print(number1 - number2)
-print(number1 * number2)
+number2 = 5 
+Addition = number1 + number2
+subtraction = number1 - number2
+multiplication = number1 * number2
+print(f"addition of {number1} and {number2} is {Addition}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"multiplication of {number1} and {number2} is {multiplication}")
